c63fb768b7066c65ccdea7f7128b44f8a3d2b8da
Neda Habibi <neda.habibi@vodafone.com>
2024-02-26 21:48:17 +0000

    edit cy.js file

--- cypress/e2e/random-quote-generator.cy.js
+++ cypress/e2e/random-quote-generator.cy.js
@@ -1,15 +1,5 @@
 describe('Random Quote Generator', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:3000');
-  });
-
   it('Random quote should be generated on page load', () => {
-    cy.get('.quote-container blockquote p').should('not.be.empty');
-    cy.get('.quote-container blockquote footer.author').should('not.be.empty');
-  });
-
-  it('Generates a new quote on button click', () => {
-    cy.get('button').click();
     cy.get('.quote-container blockquote p').should('not.be.empty');
     cy.get('.quote-container blockquote footer.author').should('not.be.empty');
   });
